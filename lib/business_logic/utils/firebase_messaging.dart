2b89bfa885b327858522174a03900d3a19675b2c
IbrahimKhattab172000 <ibrahimkhattab172000@gmail.com>
2024-03-09 17:19:21 +0200

    adjusted FirebaseMessagingUtils

--- lib/business_logic/utils/firebase_messaging.dart
+++ lib/business_logic/utils/firebase_messaging.dart
@@ -1,20 +1,69 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter_ringtone_player/flutter_ringtone_player.dart';
+import 'package:wnes_app/app/widgets/app_notification_dialog.dart';
+import 'package:wnes_app/business_logic/routing/routes.dart';
+import 'package:wnes_app/presentation/screens/notification/view.dart';
 
 class FirebaseMessagingUtils {
   final _firebaseMessaging = FirebaseMessaging.instance;
   static FirebaseMessagingUtils instance = FirebaseMessagingUtils();
 
   Future<void> initNotifications() async {
-    await _firebaseMessaging.requestPermission();
-    final fCMToken = await _firebaseMessaging.getToken();
-    print("Token: $fCMToken");
-    FirebaseMessaging.onBackgroundMessage(handleBackgroundMessage);
+    getFCMToken();
+    onMessage();
+    onMessageOpenedApp();
+    checkIfUserClickedNotificationFromBackground();
   }
-}
 
-Future<void> handleBackgroundMessage(RemoteMessage message) async {
-  print("Title: ${message.notification?.title}");
+  void onMessage() {
+    FirebaseMessaging.onMessage.listen((notification) {
+      FlutterRingtonePlayer().play(
+        android: AndroidSounds.notification,
+        ios: IosSounds.glass,
+        looping: false,
+      );
+      _handleNotificationReceiver(notification);
+    });
+  }
 
-  print("Body: ${message.notification?.body}");
-  print("PayLoad: ${message.data}");
+  Future<String?> getFCMToken() async {
+    try {
+      requestPermission();
+      final fcmToken = await _firebaseMessaging.getToken();
+      print("Token: $fcmToken");
+      return fcmToken;
+    } catch (_) {
+      return '';
+    }
+  }
+
+  Future<void> requestPermission() async {
+    _firebaseMessaging.requestPermission();
+  }
+
+  void onMessageOpenedApp() {
+    FirebaseMessaging.onMessageOpenedApp.listen((notification) {
+      RouteUtils.navigateTo(NotificationView(
+        body: notification.data,
+      ));
+    });
+  }
+
+  void checkIfUserClickedNotificationFromBackground() async {
+    final notification = await _firebaseMessaging.getInitialMessage();
+    if (notification == null) {
+      return;
+    }
+    RouteUtils.navigateAndPopAll(NotificationView(
+      body: notification.data,
+    ));
+  }
+
+  Future<void> _handleNotificationReceiver(RemoteMessage notification) async {
+    NotificationDialog.show(
+      context: RouteUtils.context,
+      title: notification.notification?.title ?? '',
+      body: notification.notification?.body ?? '',
+    );
+  }
 }
